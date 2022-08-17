e34c3ea72db9739d6a0ac4e3a3bf603115240945
Rickard Lundin <rickard@ignalina.dk>
2022-08-17 23:36:37 +0200

    fix hdfs support

---
+++ bundledImpl/HDFS_watcher.go
@@ -0,0 +1,244 @@
+/*
+ * MIT No Attribution
+ *
+ * Copyright 2022 Rickard Ernst Björn Lundin (rickard@ignalina.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package bundledImpl
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/colinmarc/hdfs/v2"
+	"github.com/colinmarc/hdfs/v2/hadoopconf"
+	"github.com/ignalina/thund/api"
+	"github.com/spf13/viper"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type HDFS struct {
+	Namenode      string
+	WatchFolder   string
+	ExcludeFolder string
+	GraceMilliSec int
+	HDFSClient    *hdfs.Client
+	Ctx           context.Context
+	User          string
+}
+
+// Process all files , and for every sucessful process , add a ".done" file.
+
+func (hdfsStruct *HDFS) Watch(eventHandlers []api.IOEvent) (bool, error) {
+	ctx := context.Background()
+	hdfsStruct.initHDFS()
+	hdfsStruct.Ctx = ctx
+	setupOk := true
+
+	for index, handler := range eventHandlers {
+		setupOk = handler.Setup(hdfsStruct)
+		if !setupOk {
+			log.Fatalln("Failed setup eventHandler nr " + strconv.Itoa(index))
+			return false, nil
+		}
+	}
+
+	for true {
+
+		fileEntityMap, err := hdfsStruct.ListFiles()
+		if nil != err {
+			return false, err
+		}
+
+		for _, fileEntity := range fileEntityMap {
+
+			//	setupOk := event.Setup()
+			//			o, _ := hdfsStruct.HDFSClient.ReadFile(fileEntity.Name)
+			var sucess bool = true
+			o, err := hdfsStruct.HDFSClient.Open(fileEntity.Name)
+			if err != nil {
+				return false, err
+			}
+
+			defer o.Close()
+			//			return ioutil.ReadAll(o)
+
+			for _, handler := range eventHandlers {
+				sucess := handler.Process(o, &fileEntity)
+				if !sucess {
+					break
+				}
+
+			}
+
+			if sucess { // create a marker file with original file name + ".done"
+				dummyFile := "Dummy file"
+				dummyFileName := fileEntity.Name + ".done"
+
+				writer, err := hdfsStruct.HDFSClient.Create(dummyFileName)
+				if nil != err {
+					return false, err
+				}
+
+				_, err = writer.Write([]byte(dummyFile))
+
+				if nil != err {
+					return false, err
+				}
+
+			}
+
+		}
+
+		time.Sleep(time.Duration(hdfsStruct.GraceMilliSec) * time.Millisecond)
+	}
+
+	return true, nil
+}
+
+func NewHDFS() *HDFS {
+	hdfs := HDFS{
+		Namenode:      viper.GetString("hdfs.namenode"),
+		WatchFolder:   viper.GetString("hdfs.watchfolder"),
+		ExcludeFolder: viper.GetString("hdfs.excludefolder"),
+		GraceMilliSec: viper.GetInt("hdfs.gracemillisec"),
+	}
+	return &hdfs
+}
+
+func (hdfsStruct HDFS) ListFiles() (map[string]api.FileEntity, error) {
+
+	objectCh, err := hdfsStruct.HDFSClient.ReadDir(hdfsStruct.WatchFolder)
+
+	if nil != err {
+		return nil, err
+	}
+
+	res_files := make(map[string]api.FileEntity)
+
+	for _, object := range objectCh {
+
+		if object.IsDir() && strings.HasPrefix(object.Name(), hdfsStruct.ExcludeFolder) {
+			continue
+		}
+
+		res_files[object.Name()] = api.FileEntity{
+			Name: object.Name(),
+			Size: object.Size(),
+		}
+	}
+
+	// remove all files that are done OR inside a exclude folder.
+	for k, _ := range res_files {
+		if _, exists := res_files[k+".done"]; exists {
+			delete(res_files, k)
+			delete(res_files, k+".done")
+		}
+	}
+	return res_files, nil
+}
+
+func (hdfsStruct *HDFS) initHDFS() *hdfs.Client {
+	//	ctx := context.Background()
+	var err error
+	hdfsStruct.HDFSClient, err = GetClient(hdfsStruct.Namenode, hdfsStruct.User)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", hdfsStruct.HDFSClient) // minioClient is now set up
+	return hdfsStruct.HDFSClient
+}
+
+func (hdfsStruct HDFS) GetFileSet() map[string]int64 {
+	return hdfsStruct.GetFilteredFileSet("")
+}
+
+func (hdfsStruct HDFS) GetFilteredFileSet(filter string) map[string]int64 {
+	m := make(map[string]int64)
+
+	entries, err := hdfsStruct.ListFiles()
+
+	if err != nil {
+		return nil
+	}
+	for _, ent := range entries {
+		m[ent.Name] = int64(ent.Size)
+	}
+
+	return m
+}
+
+var cachedClients map[string]*hdfs.Client = make(map[string]*hdfs.Client)
+
+// Code copied from colinmarc/hdfs
+func GetClient(namenode string, user string) (*hdfs.Client, error) {
+	if cachedClients[namenode] != nil {
+		return cachedClients[namenode], nil
+	}
+
+	if namenode == "" {
+		namenode = os.Getenv("HADOOP_NAMENODE")
+	}
+
+	conf, err := hadoopconf.LoadFromEnvironment()
+	if err != nil {
+		return nil, fmt.Errorf("Problem loading configuration: %s", err)
+	}
+
+	options := hdfs.ClientOptionsFromConf(conf)
+	if namenode != "" {
+		options.Addresses = strings.Split(namenode, ",")
+	}
+
+	if options.Addresses == nil {
+		return nil, errors.New("Couldn't find a namenode to connect to. You should specify hdfs://<namenode>:<port> in your paths. Alternatively, set HADOOP_NAMENODE or HADOOP_CONF_DIR in your environment.")
+	}
+
+	if options.KerberosClient != nil {
+		panic("NOT IMPLEMENTED")
+		//		options.KerberosClient, err = getKerberosClient()
+		if err != nil {
+			return nil, fmt.Errorf("Problem with kerberos authentication: %s", err)
+		}
+	} else {
+		options.User = user
+	}
+
+	// Set some basic defaults.
+	dialFunc := (&net.Dialer{
+		Timeout:   5 * time.Second,
+		KeepAlive: 5 * time.Second,
+		DualStack: true,
+	}).DialContext
+
+	options.NamenodeDialFunc = dialFunc
+	options.DatanodeDialFunc = dialFunc
+
+	c, err := hdfs.NewClient(options)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't connect to namenode: %s", err)
+	}
+
+	cachedClients[namenode] = c
+	return c, nil
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/ignalina/thund
 go 1.18
 
 require (
-	github.com/minio/minio-go/v7 v7.0.31
-	github.com/segmentio/kafka-go v0.4.33
+	github.com/colinmarc/hdfs/v2 v2.3.0
+	github.com/minio/minio-go/v7 v7.0.34
+	github.com/segmentio/kafka-go v0.4.34
 	github.com/spf13/viper v1.12.0
 )
 
@@ -12,9 +13,16 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.15.8 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
@@ -25,19 +33,18 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/rs/xid v1.4.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
-	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
